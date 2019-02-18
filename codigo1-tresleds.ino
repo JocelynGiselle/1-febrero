3e39a01c20d8999da971f4bb0552901aad5dab3c
JocelynGiselle <47005781+JocelynGiselle@users.noreply.github.com>
2019-02-17 20:42:26 -0600

    Update codigo1-tresleds.ino

--- codigo1-tresleds.ino
+++ codigo1-tresleds.ino
@@ -1,28 +1,22 @@
-#define pinLED1 8
-#define pinLED2 9
-#define pinLED3 10
+#define LED1 8
+#define LED2 9
+#define LED3 10
 
 void setup() {
 
- pinMode(pinLED1, OUTPUT);  
- pinMode(pinLED2, OUTPUT);  
- pinMode(pinLED3, OUTPUT);  
+ pinMode(LED1, OUTPUT);  
+ pinMode(LED2, OUTPUT);  
+ pinMode(LED3, OUTPUT);  
 
 }
 
 void loop() {
   
- digitalWrite(pinLED1, HIGH); // enciende
- digitalWrite(pinLED2, LOW); // apaga
- digitalWrite(pinLED3, LOW); // apaga
+ digitalWrite(LED1, HIGH); 
  delay(500);
- digitalWrite(pinLED1, LOW);   
- digitalWrite(pinLED2, HIGH); 
- digitalWrite(pinLED3, LOW); 
+ digitalWrite(LED1, LOW);    
  delay(500); 
- digitalWrite(pinLED1, LOW); 
- digitalWrite(pinLED2, LOW); 
- digitalWrite(pinLED3, HIGH); 
+ digitalWrite(LED2, LOW); 
  delay(500);
   
 }
